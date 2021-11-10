9954c64bb330b79d3eeb6610068c6e43b0782728
Madhav243 <madhav.verma018@gmail.com>
2021-11-10 12:50:04 +0530

    Added header

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { ReceipesComponent } from './receipes/receipes.component';
+import { RecipeListComponent } from './receipes/recipe-list/recipe-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    ReceipesComponent,
+    RecipeListComponent
   ],
   imports: [
     BrowserModule,
